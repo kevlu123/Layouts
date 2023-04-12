1ae28ef9fdc54687169205d429295e776dba5e75
Kevin Lu <kevlu321@gmail.com>
2023-04-12 17:45:17 +1200

    Added missing Children property to Overlay

--- Overlay.cs
+++ Overlay.cs
@@ -1,6 +1,8 @@
 namespace Layouts;
 
 public class Overlay : Container {
+    public new List<View> Children => base.Children;
+
     public override void ComputeRect(Rect rect, Rect visibleRect) {
         base.ComputeRect(rect, visibleRect);
         foreach (var child in Children) {
